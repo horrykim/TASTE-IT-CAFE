316e9d063dcef920c8e4048f483a00be7c8a82b2
Horry Kim Throy Belia <horr.belia.swu@phinmaed.com>
2026-08-09 14:29:40 +0800

    update code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {webRouter} from 'react-router-dom'
+
+export default function App() {
+  return (
+    <webRouter>
+      <div className="App">
+        <h1>Welcome to Taste It Cafe</h1>
+        <p>Discover the best flavors in town!</p>
+      </div>
+    </webRouter>
+  )
+}
